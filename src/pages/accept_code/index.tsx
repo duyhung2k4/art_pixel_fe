db3418723755c4638e1cdb8fb069283026c678fa
ndhung <nguyenduyhung04092004@gmail.com>
2024-09-24 23:54:07 +0700

    .

--- src/pages/accept_code/index.tsx
+++ src/pages/accept_code/index.tsx
@@ -1,29 +1,50 @@
 import React, { useState } from "react";
-import { Button, TextInput } from "@mantine/core";
+
+import { Button, Stack, Text, TextInput } from "@mantine/core";
 import { useAcceptCodeMutation } from "@/redux/api/auth.api";
+
+import classes from "./style.module.css";
+import { useNavigate } from "react-router";
+import { ROUTER } from "@/constant/router";
 
 
 
 const AcceptCode: React.FC = () => {
     const [code, setCode] = useState<string>("");
 
-    const [post] = useAcceptCodeMutation();
+    const navigation = useNavigate();
+    const [post, { isLoading }] = useAcceptCodeMutation();
 
     const handleAcceptCode = async () => {
         const result = await post(code);
-        console.log(result);
+        if("error" in result) return
+
+        navigation(ROUTER.LOGIN.href);
     }
 
     return (
-        <>
-            <TextInput
-                label="Code"
-                placeholder="Code"
-                value={code}
-                onChange={e => setCode(e.target.value)}
-            />
-            <Button onClick={handleAcceptCode}>Send</Button>
-        </>
+        <Stack className={classes.root}>
+            <Stack className={classes.form} w={400}>
+                <Text
+                    style={{
+                        textAlign: "center",
+                        fontSize: 20,
+                        fontWeight: 500
+                    }}
+                >Nhập mã xác nhận</Text>
+                <TextInput
+                    label="Mã xác nhận"
+                    placeholder="Mã xác nhận"
+                    value={code}
+                    onChange={e => setCode(e.target.value)}
+                />
+                <Button
+                    onClick={handleAcceptCode}
+                    loading={isLoading}
+                    disabled={isLoading}
+                >Xác nhận</Button>
+            </Stack>
+        </Stack>
     )
 }
 
